f594cad6afbe9c81c1308ab3fa2428c89327c71e
di-nethra <anjana.dinethra@gmail.com>
2021-12-17 16:05:01 +0530

    hero area completed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import "../styles/Hero.css";
+import me from "../images/me3.png";
 const useStyles = makeStyles({
   hero: {
     display: "flex",
@@ -14,6 +15,7 @@ const useStyles = makeStyles({
   },
   heroRight: {
     flex: "1",
+    position: "relative",
   },
   leftWrapepr: {
     padding: "50px",
@@ -27,6 +29,7 @@ const useStyles = makeStyles({
   },
   name: {
     fontSize: "60px",
+    fontWeight: "bold",
   },
   title: {
     height: "50px",
@@ -39,11 +42,26 @@ const useStyles = makeStyles({
     height: "50px",
     fontSize: "30px",
     fontWeight: "bold",
-    color: "#000080",
+    color: "#002450",
     display: "flex",
     alignItems: "center",
   },
   heroPara: {},
+  rightMe: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    position: "absolute",
+  },
+  imageBG: {
+    clipPath: "polygon(100% 0%, 75% 50%, 100% 100%, 25% 100%, 0% 50%, 25% 0%)",
+    backgroundColor: "#002450",
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: "0",
+    right: "0",
+  },
 });
 
 export default function Hero() {
@@ -56,7 +74,7 @@ export default function Hero() {
             style={{ justifyContent: "space-between" }}
             className={classes.leftWrapepr}
           >
-            <h2 className={classes.greeting}>Hello My name is</h2>
+            <h2 className={classes.greeting}>Hello, I am</h2>
 
             <h1 className={classes.name}>Anjana Samarakoon</h1>
             <div className={classes.title}>
@@ -76,7 +94,8 @@ export default function Hero() {
           </div>
         </div>
         <div className={classes.heroRight}>
-          <h1>Right</h1>
+          <div className={classes.imageBG}></div>
+          <img src={me} alt="myimage" className={classes.rightMe} />
         </div>
       </div>
     </div>
